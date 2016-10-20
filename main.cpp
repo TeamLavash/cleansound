d90706b54d4735b69610e51ff063f7f71edd04cb
Grecha <pinguingman@gmail.com>
2016-10-20 23:28:08 +0300

    Add wavaudiofile class

--- main.cpp
+++ main.cpp
@@ -1,21 +1,17 @@
 #include <QGuiApplication>
 #include "qmlconnector.hpp"
-
+#include "wavaudiofile.hpp"
 #include <audiofile.hpp>
 #include <audioseparator.hpp>
 #include <videofile.hpp>
 #include <videogetter.hpp>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QmlConnector myClass;
-    //
-    WavAudioFile testAF("c:\\3211.wav");
-    QVector <sample_t> testVector;
-    testAF.getDiscreteSamples(testVector);
-    qDebug() <<  testVector.length();
-    qDebug() <<  testAF.wavHeader.subchunk2Size;
+
     return app.exec();
 }
