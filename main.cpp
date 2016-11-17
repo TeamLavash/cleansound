362ae6188a4afb1903974606efa2b472b628fe79
Igor Alexanov <popzxc@yandex.ru>
2016-11-18 01:14:10 +0300

    Removed trash from main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,16 @@
 #include <QGuiApplication>
 #include "qmlconnector.hpp"
 #include "wavaudiofile.hpp"
-#include <audiofile.hpp>
-#include <audioseparator.hpp>
-#include <videofile.hpp>
-#include <videogetter.hpp>
-#include <youtubevideofile.hpp>
-#include <QDebug>
-
-#include "localvideofile.hpp"
+#include "audiofile.hpp"
+#include "audioseparator.hpp"
+#include "videofile.hpp"
+#include "videogetter.hpp"
 
 int main(int argc, char *argv[])
 {
-    YoutubeVideoFile hello("https://www.youtube.com/watch?v=op_J1fgK4Pc");
-    hello.loadVideo();
     QGuiApplication app(argc, argv);
+
+    QmlConnector myClass;
 
     return app.exec();
 }
