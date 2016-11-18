59902b99ee90b4b1c2f555b0fd7fe83067f0e430
Grecha359 <pinguiner@gmail.com>
2016-11-18 06:16:43 +0300

    Update and define all method of class wavaudiofile.

--- wavaudiofile.hpp
+++ wavaudiofile.hpp
@@ -16,75 +16,35 @@ typedef uint32_t sample_t;
 
 class WavAudioFile : public AudioFile
 {
-    struct WAVHEADER
+
+    typedef struct  WAV_HEADER
     {
-        // WAV-формат начинается с RIFF-заголовка:
+        /* RIFF Chunk Descriptor */
+        uint8_t         RIFF[4];        // RIFF Header Magic header
+        uint32_t        ChunkSize;      // RIFF Chunk Size
+        uint8_t         WAVE[4];        // WAVE Header
+        /* "fmt" sub-chunk */
+        uint8_t         fmt[4];         // FMT header
+        uint32_t        Subchunk1Size;  // Size of the fmt chunk
+        uint16_t        AudioFormat;    // Audio format 1=PCM,6=mulaw,7=alaw,     257=IBM Mu-Law, 258=IBM A-Law, 259=ADPCM
+        uint16_t        NumOfChan;      // Number of channels 1=Mono 2=Sterio
+        uint32_t        SamplesPerSec;  // Sampling Frequency in Hz
+        uint32_t        bytesPerSec;    // bytes per second
+        uint16_t        blockAlign;     // 2=16-bit mono, 4=16-bit stereo
+        uint16_t        bitsPerSample;  // Number of bits per sample
+        /* "data" sub-chunk */
+        uint8_t         Subchunk2ID[4]; // "data"  string
+        uint32_t        Subchunk2Size;  // Sampled data length
+    } wav_hdr;
 
-        // Содержит символы "RIFF" в ASCII кодировке
-        // (0x52494646 в big-endian представлении)
-        char chunkId[4];
 
-        // 36 + subchunk2Size, или более точно:
-        // 4 + (8 + subchunk1Size) + (8 + subchunk2Size)
-        // Это оставшийся размер цепочки, начиная с этой позиции.
-        // Иначе говоря, это размер файла - 8, то есть,
-        // исключены поля chunkId и chunkSize.
-        unsigned long chunkSize;
-
-        // Содержит символы "WAVE"
-        // (0x57415645 в big-endian представлении)
-        char format[4];
-
-        // Формат "WAVE" состоит из двух подцепочек: "fmt " и "data":
-        // Подцепочка "fmt " описывает формат звуковых данных:
-
-        // Содержит символы "fmt "
-        // (0x666d7420 в big-endian представлении)
-        char subchunk1Id[4];
-
-        // 16 для формата PCM.
-        // Это оставшийся размер подцепочки, начиная с этой позиции.
-        unsigned long subchunk1Size;
-
-        // Аудио формат, полный список можно получить здесь http://audiocoding.ru/wav_formats.txt
-        // Для PCM = 1 (то есть, Линейное квантование).
-        // Значения, отличающиеся от 1, обозначают некоторый формат сжатия.
-        unsigned short audioFormat;
-
-        // Количество каналов. Моно = 1, Стерео = 2 и т.д.
-        unsigned short numChannels;
-
-        // Частота дискретизации. 8000 Гц, 44100 Гц и т.д.
-        unsigned long sampleRate;
-
-        // sampleRate * numChannels * bitsPerSample/8
-        unsigned long byteRate;
-
-        // numChannels * bitsPerSample/8
-        // Количество байт для одного сэмпла, включая все каналы.
-        unsigned short blockAlign;
-
-        // Так называемая "глубиная" или точность звучания. 8 бит, 16 бит и т.д.
-        unsigned short bitsPerSample;
-
-        // Подцепочка "data" содержит аудио-данные и их размер.
-
-        // Содержит символы "data"
-        // (0x64617461 в big-endian представлении)
-        char subchunk2Id[4];
-
-        // numSamples * numChannels * bitsPerSample/8
-        // Количество байт в области данных.
-        unsigned long subchunk2Size;
-
-        // Далее следуют непосредственно Wav данные.
-    };
-    WAVHEADER wavHeader;
+    WAV_HEADER wavHeader;
     QVector<sample_t> samples;
     QString file;
 public:
     WavAudioFile(const QString &path);
-    void getDiscreteSamples(QVector<sample_t> &);
+    void getDiscreteSamples(QVector<sample_t>&);
+    void saveToFile(QString filePath);
 };
 
 #endif // WAVAUDIOFILE_H
