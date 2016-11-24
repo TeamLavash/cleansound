500e2dcc95fd09adb67b4887b9b2c214ac5bd189
Igor Alexanov <popzxc@yandex.ru>
2016-11-24 22:18:53 +0300

    Added samples writing to temp files for ICA

--- icaaudioseparator.cpp
+++ icaaudioseparator.cpp
@@ -1,8 +1,9 @@
+#include <fstream>
 #include "icaaudioseparator.hpp"
 #include "wavaudiofile.hpp"
 #include "mockaudiofile.hpp"
 
-ICAAudioSeparator::ICAAudioSeparator()
+ICAAudioSeparator::ICAAudioSeparator(AudioPtr a, AudioPtr b) : fileA(a), fileB(b)
 {
 
 }
@@ -12,7 +13,21 @@ ICAAudioSeparator::~ICAAudioSeparator()
 
 }
 
+void ICAAudioSeparator::createSamplesFile(AudioPtr file, const std::string &identifier)
+{
+    QVector<sample_t> samples;
+    file->getDiscreteSamples(samples);
+    std::ofstream out("~temp_" + identifier + ".bin", std::ios::binary);
+    for (auto sample : samples) {
+        out << sample << " ";
+    }
+    out.close();
+}
+
 auto ICAAudioSeparator::separate(AudioPtr, AudioPtr) -> QPair<AudioPtr, AudioPtr>
 {
+    createSamplesFile(fileA, "A");
+    createSamplesFile(fileB, "B");
+
     return qMakePair(AudioPtr(new MockAudioFile), AudioPtr(new MockAudioFile));
 }
--- icaaudioseparator.hpp
+++ icaaudioseparator.hpp
@@ -1,15 +1,21 @@
 #ifndef ICAAUDIOSEPARATOR_H
 #define ICAAUDIOSEPARATOR_H
 
+#include <string>
 #include "audioseparator.hpp"
 
 class ICAAudioSeparator : public AudioSeparator
 {
 public:
-    ICAAudioSeparator();
+    ICAAudioSeparator(AudioPtr a, AudioPtr b);
     virtual ~ICAAudioSeparator();
 
     QPair<AudioPtr, AudioPtr> separate(AudioPtr, AudioPtr) override;
+
+private:
+    void createSamplesFile(AudioPtr file, const std::string &identifier);
+    AudioPtr fileA;
+    AudioPtr fileB;
 };
 
 #endif // ICAAUDIOSEPARATOR_H
